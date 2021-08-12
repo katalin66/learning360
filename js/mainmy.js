817254bbe67e0e27a7c940e8bad48f8440c2f054
Katalin Szokoli <szokoli.kati@gmail.com>
2021-08-12 14:41:15 +0200

    practising bootstrap

---
+++ js/mainmy.js
@@ -0,0 +1,12 @@
+var options = {
+  animation: true
+}
+
+var element0 = document.querySelectorAll('[data-bs-toggle="tooltip"]')[0];
+var tooltip0 = new bootstrap.Tooltip(element0, options);
+
+var element1 = document.querySelectorAll('[data-bs-toggle="tooltip"]')[1];
+var tooltip1 = new bootstrap.Tooltip(element1, options);
+
+var element2 = document.querySelectorAll('[data-bs-toggle="tooltip"]')[2];
+var tooltip2 = new bootstrap.Tooltip(element2, options);
